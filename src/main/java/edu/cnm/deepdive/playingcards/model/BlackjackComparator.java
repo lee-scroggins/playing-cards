804e0765299d8c814e91547b761ba1e7ae8f521a
Lee Scroggins <lscrggns@yahoo.com>
2021-07-21 18:46:47 -0600

    Tried to create hand with card and rank.

--- src/main/java/edu/cnm/deepdive/playingcards/model/BlackjackComparator.java
+++ src/main/java/edu/cnm/deepdive/playingcards/model/BlackjackComparator.java
@@ -1,12 +1,37 @@
 package edu.cnm.deepdive.playingcards.model;
 
 import edu.cnm.deepdive.general.model.Hand;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 
-public class BlackjackComparator implements Comparator<Hand<Card>> {
+public class BlackjackComparator extends Rank implements Comparator<Hand<Card>>  {
+
+  public Card(Rank rank) {
+    this.rank = rank;
+  }
+
+  Hand<Integer> hand1 = new Hand<>();
+  Hand<Integer> hand2 = new Hand<>();
+
+  private static List<Card> getHand1(Random rng) {
+    List<Card> hand1 = new ArrayList<>();
+    for (Rank rank : Rank.values()) {
+      hand1.add(new <Card>(rank));
+    }
+    Collections.shuffle(hand1, rng);
+    return hand1;
+  }
+
+}
+
 
   @Override
   public int compare(Hand<Card> hand1, Hand<Card> hand2) {
-    return 0;
+
   }
+
+
 }
